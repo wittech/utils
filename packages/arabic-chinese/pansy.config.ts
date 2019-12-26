07e0e2135c0de52f988022b31ec8f61236f5b8c5
wangxingkang <156148958@qq.com>
2019-12-27 00:19:04 +0800

    feat: add arabic-chinese package

---
+++ packages/arabic-chinese/pansy.config.ts
@@ -0,0 +1,10 @@
+import { Config } from '@walrus/pansy';
+
+const config: Config = {
+  output: {
+    format: ['cjs', 'es', 'umd', 'umd-min'],
+    moduleName: 'isNaN'
+  }
+};
+
+export default config;
